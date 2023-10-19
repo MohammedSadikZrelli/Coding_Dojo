4f3dab3bc558ca4d3c788864f62bd9431dcd3cc1
MohammedSadikZrelli <148113793+MohammedSadikZrelli@users.noreply.github.com>
2023-10-19 09:26:13 +0100

    rien

---
+++ Coding_Dojo/Programming Basics/Week 1/Day 2/Optional/code.js
@@ -0,0 +1,8 @@
+// likeCount will start counting from 0 
+var likeCount = 0;
+//increaseslikes 
+function increaseLikes() {
+    // will increaselikes by one
+    likeCount = likeCount + 1;
+}
+
---
+++ Coding_Dojo/Programming Basics/Week 1/Day 3/Practice/sadik_zrelli_loopdloop.js
@@ -0,0 +1,17 @@
+var candy=0 // the pocket of candy
+for (var i=0;i<=6;i=i+2) {
+										// start from 0
+										// stop at 6
+										// steps 2
+										//repetition
+										//condition if he passed 2 mr
+candy = candy +1 // adding the candy to the pocket
+
+}
+
+for(var i=0;i<=6;i=i+2){
+	if(i>5.5){
+		candy = candy +1 // adding the candy to the pocket
+
+	}
+}
