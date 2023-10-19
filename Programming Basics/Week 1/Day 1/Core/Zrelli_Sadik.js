ef6fed2219a006a5f27736df74cb25f5c469ad18
MohammedSadikZrelli <148113793+MohammedSadikZrelli@users.noreply.github.com>
2023-10-19 09:30:48 +0100

    Delete Programming Basics/Week 1 directory

--- Programming Basics/Week 1/Day 1/Core/Zrelli_Sadik.js
+++
@@ -1,2 +0,0 @@
-var minimum_age=10;
-var minimum_height=42;
--- Programming Basics/Week 1/Day 2/Optional/code.js
+++
@@ -1,8 +0,0 @@
-// likeCount will start counting from 0 
-var likeCount = 0;
-//increaseslikes 
-function increaseLikes() {
-    // will increaselikes by one
-    likeCount = likeCount + 1;
-}
-
