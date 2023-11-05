c75171ca23ba093a5970e11b9c95068eee474d41
MohammedSadikZrelli <saddikzrelli@gmail.com>
2023-11-05 16:58:00 +0100

    making connection

---
+++ Coding_Dojo/WebFundamentals/Week 2/Day 2/Practice/making-connection/script.js
@@ -0,0 +1,23 @@
+
+asi=document.querySelector(".ac");
+aso=document.querySelector(".imge")
+bad=document.querySelector(".badge")
+function accept(){
+asi.remove();
+aso.remove();
+bad.innerText--;
+}
+adii=document.querySelector(".adi")
+olaa=document.querySelector(".ola")
+function accept2(){
+    adii.remove();
+    olaa.remove();
+    bad.innerText--;
+    }
+    
+
+jane=document.querySelector(".joe")
+
+function change(){
+    jane.innerHTML="Jesse Pinkman"
+}
