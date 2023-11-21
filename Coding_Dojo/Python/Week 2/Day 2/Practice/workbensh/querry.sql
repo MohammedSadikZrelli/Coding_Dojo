f26e6af5e4efad0c2e788b7cdb124ab71e4cd548
MohammedSadikZrelli <saddikzrelli10@gmail.com>
2023-11-21 19:40:37 +0100

    lol

--- Coding_Dojo/Python/Week 2/Day 2/Practice/workbensh/querry.sql
+++ Coding_Dojo/Python/Week 2/Day 2/Practice/workbensh/querry.sql
@@ -1,8 +1,14 @@
-SET SQL_SAFE_UPDATES = 0;
-SELECT * FROM my_db.names;
-insert into names (name) values ("bouhmid");
-insert into names (name) values ("abdelrahim");
-insert into names (name) values ("nawfel");
-insert into names (name) values ("ala");
-delete from names where (name="bouhmid");
-update names set name="abdelrahim" where (name="bouhmid");
+select * from users_schema.users;
+insert into users_schema.users (first_name,last_name,email) values('sadik',"zrelli","samuel@gmail.com") ;
+insert into users_schema.users (first_name,last_name,email) values('abdel',"rahim","abdelrahim@gmail.com") ;
+insert into users_schema.users (first_name,last_name,email) values('bouhmid',"hamid","bouhmid@gmail.com") ;
+select * from users_schema.users where (email="samuel@gmail.com");
+select * from users_schema.users where (id=3);
+update users_schema.users set last_name="Pancake" where id=3;
+select * from users_schema.users where (id=3);
+delete from users_schema.users where (id=2);
+select * from users_schema.users where (id=2); # rip bouhmid
+SELECT * FROM users_schema.users
+ORDER BY first_name;
+SELECT * FROM users_schema.users
+ORDER BY  first_name desc;
