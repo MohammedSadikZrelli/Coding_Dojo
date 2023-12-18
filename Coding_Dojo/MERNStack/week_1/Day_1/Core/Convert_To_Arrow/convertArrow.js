e44947dfa0df02e485451f2770ab22a1f815378b
MohammedSadikZrelli <saddikzrelli10@gmail.com>
2023-12-18 15:30:02 +0100

    convert

---
+++ Coding_Dojo/MERNStack/week_1/Day_1/Core/Convert_To_Arrow/convertArrow.js
@@ -0,0 +1,20 @@
+// function getValueFromId(id){
+//     return document.getElementById(id).value;
+// }
+const getValue=(id) => document.getElementById(id).value
+
+// function setBackgroundColorById(id, color){
+//     document.getElementById(id).style = "background-color: " + color;
+// }
+function setBackgroundColorById(id, color) {
+    document.getElementById(id).style = "background-color: " + color;
+}
+
+
+
+// function mouseOverFunction(el){
+//     el.style = "background-color: black";
+// }
+
+
+const MouseOver=(el) => {  el.style = "background-color: black"; }
