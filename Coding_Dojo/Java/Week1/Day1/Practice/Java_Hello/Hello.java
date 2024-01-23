d4cd036cebc2d2222fdb2469e67f7600dbc0f30d
MohammedSadikZrelli <saddikzrelli10@gmail.com>
2024-01-23 18:16:25 +0100

    Hello Java

---
+++ Coding_Dojo/Java/Week1/Day1/Practice/Java_Hello/Hello.java
@@ -0,0 +1,13 @@
+class Hello {
+
+    public static void main (String a[])
+    {
+        
+        System.out.print("My name is Coding Dojo\r\n" + //
+                "I am 100 years old\r\n" + //
+                "My hometown is Burbank, CA");
+    }
+
+
+}
+
