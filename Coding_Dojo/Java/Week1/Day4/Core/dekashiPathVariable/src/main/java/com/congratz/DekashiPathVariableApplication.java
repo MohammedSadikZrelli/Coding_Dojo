d662b046f01220f0239147ba9806290764c87ac2
MohammedSadikZrelli <saddikzrelli10@gmail.com>
2024-02-05 11:25:26 +0100

    dekashiVariables

---
+++ Coding_Dojo/Java/Week1/Day4/Core/dekashiPathVariable/src/main/java/com/congratz/DekashiPathVariableApplication.java
@@ -0,0 +1,13 @@
+package com.congratz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DekashiPathVariableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DekashiPathVariableApplication.class, args);
+	}
+
+}
---
+++ Coding_Dojo/Java/Week1/Day4/Core/dekashiPathVariable/src/test/java/com/congratz/DekashiPathVariableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.congratz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DekashiPathVariableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
